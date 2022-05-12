3816f6ab74712003869a167c3ea4194eedd6974e
MacMini17 <mcm-mac-17@MacMini17s-Mac-mini.local>
2022-05-12 11:31:40 -0700

    final

--- test/specs/checkboxes.spec.js
+++ test/specs/checkboxes.spec.js
@@ -1,13 +1,13 @@
 const ChexPage = require('../pageobjects/checkboxes.page');
 
 describe('My checkbox application', () => {
-    xit('should test the Checkboxes page', async () => {
+    it('should test the Checkboxes page', async () => {
         await ChexPage.open();
     });
-    xit('should confirm the presence of a heading', async () => {
+    it('should confirm the presence of a heading', async () => {
         await expect(ChexPage.heading).toBeExisting(); 
     });
-    xit('should deselect the second checkbox', async () => {
+    it('should deselect the second checkbox', async () => {
         await expect(ChexPage.checkTwo).toBeExisting(); 
         await ChexPage.checkTwo.click();
 
